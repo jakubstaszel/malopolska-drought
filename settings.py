9d9312810f56e28a01726cd3cc7df058cba9fe44
jakubstaszel <62653419+jakubstaszel@users.noreply.github.com>
2023-03-28 18:05:53 +0200

    Initial settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+# database
+DATABASE: Final = "drought"
+USER: Final = "postgres"
+PASSWORD: Final = "postgres"
+HOST: Final = "127.0.0.1"
+PORT: Final = "5432"
+
+# open access hub
+OAH_LOGIN: Final = "zuluzi"
+OAH_PASSWORD: Final = "spr@@@wdzAMYwooDE"
