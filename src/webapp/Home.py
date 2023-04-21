c5213b90303362b55070423422e536a74e7bfcd5
jakubstaszel <staszel.jakub.98@gmail.com>
2023-04-21 13:31:55 +0200

    Run streamlit app from tools

--- src/webapp/Home.py
+++ src/webapp/Home.py
@@ -3,19 +3,8 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-# Customize the sidebar
-markdown = """
-Web App URL: <https://template.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Drought Monitoring for Malopolska Region")
 
 st.markdown(
     """
